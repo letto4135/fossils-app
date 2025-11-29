756e59483e244e5f2c10c72753e36861eefc83f4
Alex Life <alex@mister-life.com>
2025-11-29 12:08:33 -0500

    use filebrowser as base

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 # Use a recent Alpine for compatibility
-FROM alpine:latest
-
+FROM filebrowser/filebrowser:v2-s6
 # Install build dependencies and build Fossil from trunk
 RUN apk update && \
-    apk upgrade && \
-    apk add git openssh && \
+    apk upgrade
+
+RUN apk add git openssh && \
     apk add --no-cache curl gcc make tcl musl-dev openssl-dev zlib-dev openssl-libs-static zlib-static && \
     curl "https://fossil-scm.org/home/tarball/fossil-src.tar.gz?name=fossil-src&uuid=trunk" -o fossil-src.tar.gz && \
     tar xf fossil-src.tar.gz && \
@@ -17,8 +17,7 @@ RUN apk update && \
     apk del curl gcc make tcl musl-dev zlib-dev
 
 EXPOSE 8080
-VOLUME /fossils
-COPY run.sh /run.sh
-RUN chmod +x /run.sh
-# Run the server on all interfaces, serving the mounted directory
-CMD ["/run.sh"]
+COPY --chmod=755 run.sh ./
+
+ENTRYPOINT ["/bin/sh"]
+CMD ["run.sh"]
--- run.sh
+++ run.sh
@@ -1,5 +1,73 @@
 #!/bin/sh
 
+set -e
+
+if [ -z "$RAILWAY_VOLUME_MOUNT_PATH" ]; then
+    echo "no volume attached, please attach a volume"
+    echo "upon attaching a volume let this service rebuild"
+    exit 0
+fi
+
+if [ -z "$WEB_USERNAME" ]; then
+    echo "missing the WEB_USERNAME variable, please add it to continue"
+    exit 0
+fi
+
+if [ -z "$WEB_PASSWORD" ]; then
+    echo "missing the WEB_PASSWORD variable, please add it to continue"
+    exit 0
+fi
+
+if [ -f "/.filebrowser.json" ]; then
+  rm /.filebrowser.json
+fi
+
+FILEBROWSER_DATA_PATH=$RAILWAY_VOLUME_MOUNT_PATH/appdata/filebrowser
+
+DATABASE_PATH=$FILEBROWSER_DATA_PATH/filebrowser.db
+
+FILEBROWSER_USERNAME_PATH=$FILEBROWSER_DATA_PATH/username
+
+if [ "$USE_VOLUME_ROOT" == "1" ]; then
+    echo "using volume root as storage location"
+    FILEBROWSER_STORAGE_PATH=$RAILWAY_VOLUME_MOUNT_PATH
+else
+    FILEBROWSER_STORAGE_PATH=$RAILWAY_VOLUME_MOUNT_PATH/storage
+    if [ ! -d $FILEBROWSER_STORAGE_PATH ]; then
+        mkdir $FILEBROWSER_STORAGE_PATH
+    fi
+fi
+
+if [ -f "$DATABASE_PATH" ]; then
+    if [ -f "$FILEBROWSER_USERNAME_PATH" ]; then
+        FILEBROWSER_CURRENT_USERNAME=$(cat $FILEBROWSER_USERNAME_PATH)
+
+        if [[ -n "$FILEBROWSER_CURRENT_USERNAME" && "$FILEBROWSER_CURRENT_USERNAME" != "$WEB_USERNAME" ]]; then
+            echo "new username was set in the service variables, changing username: $FILEBROWSER_CURRENT_USERNAME -> $WEB_USERNAME"
+            /filebrowser users update $FILEBROWSER_CURRENT_USERNAME --username $WEB_USERNAME --database $DATABASE_PATH > /dev/null 2>&1
+            echo $WEB_USERNAME >| $FILEBROWSER_USERNAME_PATH
+            echo "username updated"
+        fi
+    fi
+else
+    echo "first start, creating database"
+    /filebrowser config init --database $DATABASE_PATH > /dev/null 2>&1
+
+    echo "setting configurations"
+    /filebrowser config set --address "0.0.0.0" --database $DATABASE_PATH > /dev/null 2>&1
+
+    /filebrowser users add $WEB_USERNAME $WEB_PASSWORD --database $DATABASE_PATH > /dev/null 2>&1
+fi
+
+echo $WEB_USERNAME > $FILEBROWSER_USERNAME_PATH
+
+/filebrowser users update $WEB_USERNAME --password $WEB_PASSWORD --database $DATABASE_PATH > /dev/null 2>&1
+
+/filebrowser config set --port $PORT --database $DATABASE_PATH > /dev/null 2>&1
+/filebrowser config set --root $FILEBROWSER_STORAGE_PATH --database $DATABASE_PATH > /dev/null 2>&1
+
+/filebrowser version
+
 # Create the .ssh directory and set permissions
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
@@ -17,5 +85,6 @@ chmod 600 /root/.ssh/config
 echo $SSH_PRIVATE_KEY > /root/.ssh/gh_mirror_key
 chmod 600 /root/.ssh/gh_mirror_key
 
-# Run the server
-fossil server --repolist --https /fossils/repos --port 8080
+# Run the servers
+fossil server --repolist --https /fossils/repos --port 8080 &
+/filebrowser --database $DATABASE_PATH 2>&1
